45f830f5f99369094596639a9500d6632b018fcf
Pradeep717 <kumara_dmpsu_e21@engug.ruh.ac.lk>
2025-01-11 23:00:55 +0530

    finish authentication using clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton />
+        <SignInButton mode="modal" >
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign In
+          </button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
