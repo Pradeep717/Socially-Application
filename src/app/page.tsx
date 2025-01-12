80a7f0b5a7318c4ab1fb085f24bfbd000db1a3c9
Pradeep717 <kumara_dmpsu_e21@engug.ruh.ac.lk>
2025-01-12 23:44:58 +0530

    finish authentication using clerk new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode="modal" >
+        <SignInButton mode="modal">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Sign In
+            Sign in with Clerk
           </button>
         </SignInButton>
       </SignedOut>
