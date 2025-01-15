784262db7b9d055c850bda8c73a482381fbee680
Pradeep717 <kumara_dmpsu_e21@engug.ruh.ac.lk>
2025-01-15 23:29:06 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <SignedOut>
         <SignInButton mode="modal">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Sign in with Clerk
+            Sign in
           </button>
         </SignInButton>
       </SignedOut>
