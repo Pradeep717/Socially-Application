2d8b4e7e1806f1bbc5688408a6005ec49e4eda25
Pradeep717 <kumara_dmpsu_e21@engug.ruh.ac.lk>
2025-01-17 23:33:05 +0530

    add shadcnn button ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
@@ -5,9 +6,9 @@ export default function Home() {
     <div>
       <SignedOut>
         <SignInButton mode="modal">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <Button>
             Sign in
-          </button>
+          </Button>
         </SignInButton>
       </SignedOut>
       <SignedIn>
